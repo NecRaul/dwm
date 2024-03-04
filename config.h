bbf8ba6be5196bb421a1da20a06348fccb062edd
NecRaul <necraul@kuroneko.dev>
2024-03-04 04:27:54 +0400

    Added fonts, increased gaps

--- config.h
+++ config.h
@@ -16,15 +16,18 @@ static const unsigned int maxHTab = 200;      /* tab menu height */
 /* appearance */
 static unsigned int borderpx = 3; /* border pixel of windows */
 static unsigned int snap = 32;    /* snap pixel */
-static unsigned int gappih = 10;  /* horiz inner gap between windows */
-static unsigned int gappiv = 5;   /* vert inner gap between windows */
-static unsigned int gappoh = 5;   /* horiz outer gap between windows and screen edge */
-static unsigned int gappov = 15;  /* vert outer gap between windows and screen edge */
+static unsigned int gappih = 20;  /* horiz inner gap between windows */
+static unsigned int gappiv = 10;  /* vert inner gap between windows */
+static unsigned int gappoh = 10;  /* horiz outer gap between windows and screen edge */
+static unsigned int gappov = 30;  /* vert outer gap between windows and screen edge */
 static int swallowfloating = 0;   /* 1 means swallow floating windows by default */
 static int smartgaps = 0;         /* 1 means no outer gap when there is only one window */
 static int showbar = 1;           /* 0 means no bar */
 static int topbar = 1;            /* 0 means bottom bar */
-static char *fonts[] = {"HackNerdFontMono:size=10"};
+static char *fonts[] = {
+    "monospace:size=10",
+    "HackNerdFontMono:size=10",
+    "NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"};
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#444444";
 static char normfgcolor[] = "#bbbbbb";
@@ -244,3 +247,4 @@ static const Button buttons[] = {
     {ClkTagBar, 0, Button5, shiftview, {.i = 1}},
     {ClkRootWin, 0, Button2, togglebar, {0}},
 };
+
